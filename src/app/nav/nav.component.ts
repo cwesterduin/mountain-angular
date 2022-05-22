5c5b9041c4f06d43dca329903c648574b2a9dd0a
chris westerduin <chriswesterduin@outlook.com>
2022-05-22 18:42:24 +0000

    misc styling

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnChanges, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthenticatorService} from "@aws-amplify/ui-angular";
 import {Amplify, Auth} from "aws-amplify";
 import awsExports from "../../aws-exports";
+
 
 @Component({
   selector: 'app-nav',
@@ -12,17 +13,20 @@ import awsExports from "../../aws-exports";
 export class NavComponent implements OnInit {
   constructor(
     private router: Router,
-    public authenticator: AuthenticatorService
+    public authenticator: AuthenticatorService,
+    private ElByClassName: ElementRef
   ) {
     Amplify.configure(awsExports)
   }
 
   signedIn = true;
+  popper = 0
 
   async signOut() {
     try {
       await Auth.signOut();
       await this.router.navigate(["/"]);
+      this.signedIn = false;
     } catch (error) {
       console.log('error signing out: ', error);
     }
@@ -33,9 +37,47 @@ export class NavComponent implements OnInit {
     this.authenticated()
   }
 
+
   authenticated() {
-    Auth.currentUserInfo().then(res=>{
-      this.signedIn = !!res
-    })
+    try {
+      Auth.currentUserInfo().then(res => {
+        console.log(res)
+        this.signedIn = !!res
+      })
+    } catch {
+      this.signedIn = false
+    }
   }
+
+  // services = {
+  //   that: this,
+  //   async handleSignIn(formData: Record<string, any>,) {
+  //     let {username, password } = formData;
+  //     // custom username
+  //     username = username.toLowerCase();
+  //     let x  = await Auth.signIn({
+  //       username,
+  //       password,
+  //     });
+  //     if (x) {
+  //       this.that.signedIn = true;
+  //       return x
+  //     }
+  //   },
+  // }
+  // pop(){
+  //   setInterval(() => {
+  //     console.log("hello")
+  //     let btnElements = (<HTMLElement>this.ElByClassName.nativeElement).querySelectorAll(
+  //       '.amplify-button'
+  //     );
+  //     console.log(btnElements.length)
+  //     for (let i = 0; i < btnElements.length; i++) {
+  //       btnElements[i].className = "mat-raised-button mat-focus-indicator mat-button-base mat-primary"
+  //       btnElements[i].setAttribute("mat-raised-button", "")
+  //     }
+  //   }, 1)
+  // }
+
+
 }
