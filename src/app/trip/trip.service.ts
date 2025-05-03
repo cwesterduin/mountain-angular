7c7d0e9b01efea79e034682346067276ff58428c
chris <chriswesterduin@outlook.com>
2025-05-03 12:53:52 +0100

    small fixes

--- src/app/trip/trip.service.ts
+++ src/app/trip/trip.service.ts
@@ -20,7 +20,7 @@ export class TripService {
   }
 
   getOneTrip(id: string) {
-    return this.http.get<any>(this.configUrl +  + "/" + id).pipe(
+    return this.http.get<any>(this.configUrl +  "/" + id).pipe(
       catchError(ResponseHelpers.handleError)
     );
   }
